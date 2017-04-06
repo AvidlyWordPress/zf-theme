5cd6ff2e9037337a8a9073a7753da8177cfc51a8
Daniel Koskinen <dani@dani.fi>
2017-04-06 08:44:15 +0300

    Update WP gallery code for Foundation 6, finally.
    Improve code formatting.

--- functions/foundation.php
+++ functions/foundation.php
@@ -29,14 +29,14 @@ function zf_theme_foundation_adminbar_styles() {
  * @return string HTML content to display gallery.
  */
 add_filter( 'post_gallery', 'zf_theme_foundation_gallery_shortcode', 10, 2 );
-function zf_theme_foundation_gallery_shortcode($defaults = '', $attr) {
+function zf_theme_foundation_gallery_shortcode( $defaults = '', $attr ) {
 	global $post;
 
 	static $instance = 0;
 	$instance++;
 
 	// Allow plugins/themes to override the default gallery template.
-	$output = apply_filters('zf_theme_post_gallery', '', $attr);
+	$output = apply_filters( 'zf_theme_post_gallery', '', $attr );
 	if ( $output != '' )
 		return $output;
 
@@ -47,7 +47,7 @@ function zf_theme_foundation_gallery_shortcode($defaults = '', $attr) {
 			unset( $attr['orderby'] );
 	}
 
-	extract(shortcode_atts(array(
+	extract( shortcode_atts( array(
 		'order'      => 'ASC',
 		'orderby'    => 'menu_order ID',
 		'id'         => $post->ID,
@@ -60,11 +60,11 @@ function zf_theme_foundation_gallery_shortcode($defaults = '', $attr) {
 		'exclude'    => ''
 	), $attr));
 
-	$id = intval($id);
+	$id = intval( $id );
 	if ( 'RAND' == $order )
 		$orderby = 'none';
 
-	if ( !empty($include) ) {
+	if ( ! empty( $include ) ) {
 		$include = preg_replace( '/[^0-9,]+/', '', $include );
 		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
 
@@ -74,25 +74,135 @@ function zf_theme_foundation_gallery_shortcode($defaults = '', $attr) {
 		}
 	} elseif ( !empty($exclude) ) {
 		$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
-		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+		$attachments = get_children( array( 
+					'post_parent' => $id, 
+					'exclude' => $exclude, 
+					'post_status' => 'inherit', 
+					'post_type' => 'attachment', 
+					'post_mime_type' => 'image', 
+					'order' => $order, 
+					'orderby' => $orderby,
+				) );
 	} else {
-		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+		$attachments = get_children( array( 
+					'post_parent' => $id, 
+					'post_status' => 'inherit', 
+					'post_type' => 'attachment', 
+					'post_mime_type' => 'image', 
+					'order' => $order, 
+					'orderby' => $orderby,
+				) );
 	}
 
-	if ( empty($attachments) )
+	if ( empty($attachments) ) {
 		return '';
+	}
 
 	if ( is_feed() ) {
 		$output = "\n";
-		foreach ( $attachments as $att_id => $attachment )
-			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		foreach ( $attachments as $att_id => $attachment ) {
+			$output .= wp_get_attachment_link( $att_id, $size, true ) . "\n";
+		}
 		return $output;
 	}
 
-	$itemtag = tag_escape($itemtag);
-	$captiontag = tag_escape($captiontag);
-	$columns = intval($columns);
-	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+	$itemtag = tag_escape( $itemtag );
+	$captiontag = tag_escape( $captiontag );
+	$columns = intval( $columns );
+	$itemwidth = $columns > 0 ? floor( 100 / $columns ) : 100;
+	$float = is_rtl() ? 'right' : 'left';
+
+	$selector = "gallery-{$instance}";
+
+	global $post;
+
+	static $instance = 0;
+	$instance++;
+
+	// Allow plugins/themes to override the default gallery template.
+	$output = apply_filters( 'customizer_test_post_gallery', '', $attr );
+	if ( $output != '' )
+		return $output;
+
+	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( ! $attr['orderby'] ) {
+			unset( $attr['orderby'] );
+		}
+	}
+
+	extract( shortcode_atts( array(
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post->ID,
+		'itemtag'    => 'li',
+		'icontag'    => '',
+		'captiontag' => 'div',
+		'columns'    => 3,
+		'size'       => 'thumbnail',
+		'include'    => '',
+		'exclude'    => '',
+	), $attr) );
+
+	$id = intval( $id );
+	if ( 'RAND' == $order ) {
+		$orderby = 'none';
+	}
+
+	if ( !empty( $include ) ) {
+		$include = preg_replace( '/[^0-9,]+/', '', $include );
+		$_attachments = get_posts( array(
+					'include' => $include, 
+					'post_status' => 'inherit', 
+					'post_type' => 'attachment', 
+					'post_mime_type' => 'image', 
+					'order' => $order, 
+					'orderby' => $orderby,
+				) );
+
+		$attachments = array();
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[ $val->ID ] = $_attachments[ $key ];
+		}
+	} elseif ( !empty( $exclude ) ) {
+		$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+		$attachments = get_children( array(
+					'post_parent' => $id, 
+					'exclude' => $exclude, 
+					'post_status' => 'inherit', 
+					'post_type' => 'attachment', 
+					'post_mime_type' => 'image', 
+					'order' => $order, 
+					'orderby' => $orderby,
+				) );
+	} else {
+		$attachments = get_children( array(
+					'post_parent' => $id, 
+					'post_status' => 'inherit', 
+					'post_type' => 'attachment', 
+					'post_mime_type' => 'image', 
+					'order' => $order, 
+					'orderby' => $orderby,
+				) );
+	}
+
+	if ( empty( $attachments ) ) {
+		return '';
+	}
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment ) {
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		}
+		return $output;
+	}
+
+	$itemtag = tag_escape( $itemtag );
+	$captiontag = tag_escape( $captiontag );
+	$columns = intval( $columns );
+	$itemwidth = $columns > 0 ? floor( 100/$columns ) : 100;
 	$float = is_rtl() ? 'right' : 'left';
 
 	$selector = "gallery-{$instance}";
@@ -102,15 +212,15 @@ function zf_theme_foundation_gallery_shortcode($defaults = '', $attr) {
 	$small_columns = $columns > 2 ? 2 : $columns;
 	$medium_columns = $columns > 4 ? 4 : $columns;
 
-	$gallery_div = "<div class=\"gallery-wrap\"><ul id='$selector' class='small-block-grid-{$small_columns} medium-block-grid-{$medium_columns} large-block-grid-{$columns} gallery galleryid-{$id} gallery-size-{$size_class}'>";
+	$gallery_div = "<div class=\"gallery-wrap\"><ul id='$selector' class='no-bullet row small-up-{$small_columns} medium-up-{$medium_columns} large-up-{$columns} gallery galleryid-{$id} gallery-size-{$size_class}'>";
 	$output = "\n\t\t" . $gallery_div;
 
 	$i = 0;
 	foreach ( $attachments as $id => $attachment ) {
-		$thumb_src = wp_get_attachment_thumb_url($id);
-		$link_url = wp_get_attachment_url($id);
+		$thumb_src = wp_get_attachment_thumb_url( $id );
+		$link_url = wp_get_attachment_url( $id );
 
-		$has_caption = $captiontag && trim($attachment->post_excerpt) ? true : false;
+		$has_caption = $captiontag && trim( $attachment->post_excerpt ) ? true : false;
 
 		$link = "<a href='$link_url' class='gallery-item__link'><img src='$thumb_src' class='gallery-item__thumbnail' ";
 
@@ -120,7 +230,7 @@ function zf_theme_foundation_gallery_shortcode($defaults = '', $attr) {
 
 //        $aria_labelledby = $has_caption ? " aria-labelledby='img-caption-{$attachment->ID}'" : '';
 
-		$output .= "<{$itemtag} class='gallery-item'>";
+		$output .= "<{$itemtag} class='column column-block gallery-item'>";
 		$output .= "
 				$link
 		";
