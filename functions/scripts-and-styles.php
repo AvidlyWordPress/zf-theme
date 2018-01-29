f13da8192a68b71eaf9ca8f7ef8232db30ea19ec
Daniel Koskinen <dani@dani.fi>
2018-01-29 14:33:11 +0200

    Coding standards: whitespace

--- functions/scripts-and-styles.php
+++ functions/scripts-and-styles.php
@@ -13,7 +13,7 @@ function zf_theme_scripts() {
 	// Put your custom styles in style.css and uncomment the below if you're not using the sass version.
 	// wp_enqueue_style( 'zf-theme-custom-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'zf-theme-app-js', $asset_directory . 'js/app.js', array('jquery'), $version, true );
+	wp_enqueue_script( 'zf-theme-app-js', $asset_directory . 'js/app.js', array( 'jquery' ), $version, true );
 
 	wp_enqueue_script( 'zf-theme-skip-link-focus-fix', $asset_directory . 'wp-js/skip-link-focus-fix.js', array(), '20151215', true );
 
